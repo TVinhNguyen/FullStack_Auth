2accde1980a85e2b29a9c06e63f27e31fb52b16c
John Doe <johndoe@example.com>
2024-09-16 01:17:28 +0700

    second commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./db/connectDB.js";
+
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("hello world 123");
+});
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("server is running : ", PORT);
+});
